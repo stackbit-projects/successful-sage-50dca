16ef0459e197d44d03a3ab2bc27dbcc2895b88fa
Stackbit <projects@stackbit.com>
2020-12-24 23:53:33 +0200

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+echo "stackbit-build.sh: start build"
+
+# build site
+npm run build
+
+./inject-stackbit-widget.js public https://localhost:8092/init.js 5fe50dca5a410d66661c73fd
+
+echo "stackbit-build.sh: finished build"
